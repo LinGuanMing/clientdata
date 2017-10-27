e90ab825c29393d4db3f74831905f5667590aeb4
LinGuanMing <solitaryseal@gmail.com>
2017-10-27 20:22:18 +0800

    加入專案檔案。

---
+++ clientdata/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(clientdata.Startup))]
+namespace clientdata
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
